4f02d1ce71e8f30349866caa982e83996713db8d
Lucas Eckman <lucas.eckman@gmail.com>
2019-03-01 12:58:58 -0500

    part 3 rest routes and dumb model

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 import "dotenv/config";
-import cors from "cors";
 import express from "express";
+
+import models from "./models";
+import routes from "./routes";
+
+import cors from "cors";
+import bodyParser from "body-parser";
 
 const app = express();
 
 app.use(cors());
 // Before production, set up cors whitelisting
 // https://github.com/expressjs/cors
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  req.context = {
+    models,
+    me: models.users[1]
+  };
+  next();
+});
+
+app.use("/session", routes.session);
+app.use("/users", routes.user);
+app.use("/messages", routes.message);
 
 app.get("/", (req, res) => {
   res.send("hello world!");
